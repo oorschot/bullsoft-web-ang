afd76c8cb014d49750f23cd91218e692bf10e181
josvanoorschot <josvanoorschot7759@gmail.com>
2018-09-03 11:16:44 +0200

    Auto-generated commit

---
+++ out-tsc/src/app/app.module.js
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule.decorators = [
+        { type: NgModule, args: [{
+                    declarations: [
+                        AppComponent
+                    ],
+                    imports: [
+                        BrowserModule
+                    ],
+                    providers: [],
+                    bootstrap: [AppComponent]
+                },] },
+    ];
+    return AppModule;
+}());
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
